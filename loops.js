0577aa37c1deb097f7aefd781271cf9f23d7f43a
Josue V. Alorro <joalorro@gmail.com>
2018-06-06 14:20:30 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,13 @@
 var arr = []
 
-for (let i = 0 ; i < 25 ; )
+for (let i = 0 ; i < 25 ; i++) {
+  
+  if (i === 0 ) {
+    
+    arr[i] = 'I am 1 strange loop.'
+    
+  } else {
+    arr[i] = `I am ${i} strange loops.`
+  }
+  
+}
